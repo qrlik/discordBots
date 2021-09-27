e99e6c4fcf221b2e872c2ff7f34a5f30250d6728
qrlik <qrlikk@gmail.com>
2021-09-27 22:32:19 +0300

    fix

--- utils.py
+++ utils.py
@@ -7,7 +7,8 @@ def loadJsonFile(filename):
     try:
         with open(filename + '.json') as infile:
             return json.load(infile)
-    except:
+    except Exception as e:
+        print('utils: loadJsonFile: ' + str(e))
         return None
 
 def saveJsonFile(filename, data):
